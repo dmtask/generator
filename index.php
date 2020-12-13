e18c282d6e56ffc1f33b457986ac134a6b5b8f23
Daniel Mertgen <dm@eoceo.de>
2020-12-13 15:18:29 +0100

    Bisschen Layouting

--- index.php
+++ index.php
@@ -2,17 +2,27 @@
 <html lang="de">
   <head>
     <meta charset="utf-8" />
-    <link rel="icon" href="favicon.ico" />
+    <link rel="icon" href="assets/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Generator" />
+    <meta name="description" content="Passwort Generator - Einfach sichere Passwörter erstellen" />
+    <meta name="keywords" content="Passwort, Passwort erstellen, erstellen, sicher, Passwörter, sichere Passwörter, generieren, passwort generator"/>
+    <meta name="author" content="Daniel Mertgen (dm-task.de)" />
+    <meta name="robots" content="index"/>
     <title>Passwort Generator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link href="assets/index.css" rel="stylesheet" />
   </head>
   <body>
     <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <b>Willkommen!</b> - Hier kannst du dir automatisch Passwörter generieren lassen. Als Voreinstellung besteht das Passwort aus 8 Zeichen, inkl. Zahlen und Sonderzeichen. Diese Einstellung kannst du 
+          unten ändern.
+        </div>
+      </div>
       <div class="row mt-4">
         <div class="col-3">
-          <input class="form-control passwortLength" type="text" placeholder="Passwort Länge" />
+          <input class="form-control passwortLength" type="text" placeholder="Passwort Länge (8 Zeichen)" />
         </div>
         <div class="form-check col-3">
           <input class="form-check-input" type="checkbox" value="" checked=checked id="checkboxChars">
@@ -44,9 +54,12 @@
         </div>
       </div>
     </div>
+    <footer>
+      Powered by <a href="http://dm-task.de" target="_blank">dm-task.de</a>
+    </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">;</script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js">;</script>
     <script type="text/javascript" src="generator.js">;</script>
   </body>
 </html>
